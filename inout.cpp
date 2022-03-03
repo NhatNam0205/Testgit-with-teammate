e921e0d29a5eb14ca71a640735e9e92ec10cfc3d
NhatNam <ncnnam21@clc.fitus.edu.vn>
2022-03-03 16:40:48 +0700

    new in/out

--- inout.cpp
+++ inout.cpp
@@ -1,18 +1,14 @@
 #include <headerfile.h>
  
 void addfirst(node *&head, node *&tail, int x){
-    if(head != nullptr){
-        tail->next = new node;
-        tail = tail->next;
-    }
     if(head == nullptr){
         head = new node;
         tail = head;
     }
-    if(head != nullptr){
-        tail->value = x;
-        tail->next = nullptr;
-    }
+    tail->value = x;
+    tail->next = nullptr;
+    tail = tail->next;
+    if(x != 0) tail = new node;
 } 
 
 void display(node *head){
